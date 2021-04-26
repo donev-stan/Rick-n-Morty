96bac8fe413bb236cb3d1abf17c41606573474cb
donev-stan <77002111+donev-stan@users.noreply.github.com>
2021-04-26 06:28:46 +0300

    fixed bugs

--- scripts/router.js
+++ scripts/router.js
@@ -1,21 +1,44 @@
-import { characters, locations } from "./services.js";
+import { characters, locations, configureCharactersURL, configureLocationsURL } from "./services.js";
 
 const router = Sammy("#root", function () {
     this.use("Handlebars", "hbs");
 
+    let searchQuery = '';
+    let isSearching = false;
+
     // Home
     this.get("/", function (context) {
+        searchQuery = '';
         this.partial("../templates/home.hbs");
     });
 
     this.get("/home", function (context) {
+        searchQuery = '';
         this.partial("../templates/home.hbs");
     });
 
+
     // Characters
     this.get("/characters/:pageID", async function (context) {
-        const { pageID } = context.params;
-        const { info, results } = await characters.getAllCharacters(pageID);
+        let { pageID, characterName, status, gender } = context.params;
+
+        // Logic to figure out if user is searching
+        if (characterName, status, gender) isSearching = true;
+
+        const { url, returnedSearchQuery } = configureCharactersURL({ pageID, characterName, status, gender });
+        
+        if (isSearching) {
+            searchQuery = returnedSearchQuery;
+            isSearching = false;
+        }
+        if (context.path === '/characters/1') searchQuery = '';
+
+        console.log(url + searchQuery);
+        const { info, results } = await characters.getCharacters(url + searchQuery);
+        // const returned = await characters.getCharacters(url + searchQuery);
+        // const {info, results} = returned;
+        // console.log(info);
+        // console.log(results);
 
         // Getting indexes only form URLs
         Object.entries(results).map(([id, { location, origin }]) => {
@@ -29,17 +52,19 @@ const router = Sammy("#root", function () {
                     : "";
         });
 
-        context.characters = await results;
+        // Attach characters to the context
+        context.characters = results;
 
         loadPageNavigation(context, info, "characters").then(function () {
-            this.partial("../templates/characters.hbs");
+            this.partial("../templates/Characters/characters.hbs");
         });
     });
 
     // Specific Character
     this.get("/character/:characterID", async function (context) {
         const { characterID } = context.params;
-        const character = await characters.getCharacter(characterID);
+        const character = await characters.getCharactersByID(characterID);
+
 
         // Getting indexes only form URLs
         character.origin.url =
@@ -52,20 +77,30 @@ const router = Sammy("#root", function () {
                 : "";
 
         context.character = await character;
-
-        this.partial("../templates/character.hbs");
+        this.partial("../templates/Characters/character.hbs");
     });
 
     // Locations
     this.get("/locations/:pageID", async function (context) {
-        const { pageID } = context.params;
+        const { pageID, locationName, type, dimension } = context.params;
 
-        const { info, results } = await locations.getAllLocations(pageID);
+        if (locationName, type, dimension) isSearching = true;
+
+        const { url, returnedSearchQuery } = configureLocationsURL({ pageID, locationName, type, dimension });
+
+        if (isSearching) {
+            searchQuery = returnedSearchQuery;
+            isSearching = false;
+        }
+        if (context.path === '/locations/1') searchQuery = '';
+        
+        console.log(url + searchQuery);
+        const { info, results } = await locations.getLocations(url + searchQuery);
 
         context.locations = await results;
 
         loadPageNavigation(context, info, "locations").then(function () {
-            this.partial("../templates/locations.hbs");
+            this.partial("../templates/Locations/locations.hbs");
         });
     });
 
@@ -73,41 +108,68 @@ const router = Sammy("#root", function () {
     this.get("/location/:locationID", async function (context) {
         const { locationID } = context.params;
 
-        const location = await locations.getLocation(locationID);
+        const location = await locations.getLocationsByID(locationID);
 
         const residentsIDs = location.residents.map(resident => (resident ? resident.match(/\d+/)[0] : null));
 
-        const results = await characters.getSpecificCharacters(residentsIDs);
+        const results = await characters.getCharactersByID(residentsIDs);
 
-        // Getting indexes only form URLs
-        Object.entries(results).map(([id, { location, origin }]) => {
-            origin.url = origin.url != "" 
-                ? origin.url.match(/\d+/)[0] 
-                : "";
+        // // Getting indexes only form URLs
+        // Object.entries(results).map(([id, { location, origin }]) => {
+        //     origin.url = origin.url != "" 
+        //         ? origin.url.match(/\d+/)[0] 
+        //         : "";
 
-            location.url =
-                location.url != "" 
-                    ? location.url.match(/\d+/)[0] 
-                    : "";
-        });
+        //     location.url =
+        //         location.url != "" 
+        //             ? location.url.match(/\d+/)[0] 
+        //             : "";
+        // });
+        
+        // Check if a single character is returned
+        if (!results.length) {
+            const arr = [];
+            arr.push(results);
+            context.residents = arr;
+        } else {
+            context.residents = results;
+        }
 
-        context.residents = results;
         context.location = location;
-        this.partial("../templates/location.hbs");
+        this.partial("../templates/Locations/location.hbs");
     });
 
-    // Search Characters
-    // this.post('/searchCharacter', function(context) {
-    //     const data = context.params;
-    //     console.log(data);
+    // Episodes
+    this.get("/episodes/:pageID", function(context) {
+        const { pageID, episodeName, episodeCode } = context.params;
 
-    //     this.partial("../templates/characters.hbs");
-    // });
+        if (episodeName, episodeCode) isSearching = true;
+
+        const { url, returnedSearchQuery } = configureEpisodesURL({ pageID, episodeName, episodeCode });
+
+        if (isSearching) {
+            searchQuery = returnedSearchQuery;
+            isSearching = false;
+        }
+        if (context.path === '/episodes/1') searchQuery = '';
+
+        const { info, results } = 
+
+        // context.episodes = 
+
+        loadPageNavigation(context, info, "episodes").then(function () {
+            this.partial("../templates/Episodes/episodes.hbs");
+        });
+    });
+
 });
 
 function loadPageNavigation(context, { prev, next }, endpoint) {
     context.previousPage = prev ? `/${endpoint}/` + prev.match(/\d+/)[0] : null;
     context.nextPage = next ? `/${endpoint}/` + next.match(/\d+/)[0] : null;
+    
+    if (!prev && !next) context.isMorePages = false;
+    else context.isMorePages = true;
 
     return context.loadPartials({
         pageNavigation: "../templates/pageNavigation.hbs",
--- scripts/services.js
+++ scripts/services.js
@@ -1,39 +1,91 @@
 const baseURL = `https://rickandmortyapi.com/api`;
 
+function configureCharactersURL({pageID, characterName, status, gender}) {
+    const url = `${baseURL}/character/?page=${pageID}`;
+    let nameQuery, statusQuery, genderQuery = '';
+   
+    if (characterName !== undefined) {
+        if (characterName !== '') {
+            nameQuery = `&name=${characterName}`;
+        }
+    }
+
+    if (status !== undefined) {
+        if (status !== 'Status') {
+            statusQuery = `&status=${status}`;
+        }
+    }
+    
+    if (gender !== undefined) {
+        if (gender !== 'Gender') {
+            genderQuery = `&gender=${gender}`;
+        }
+    }
+    
+    const returnedSearchQuery = `${nameQuery ? nameQuery : ''}${statusQuery ? statusQuery : ''}${genderQuery ? genderQuery : ''}`;
+    return { url, returnedSearchQuery };
+}
+
+function configureLocationsURL({pageID, locationName, type, dimension}) {
+    const url = `${baseURL}/location/?page=${pageID}`;
+    let nameQuery, typeQuery, dimensionQuery = '';
+
+    if (locationName !== undefined) {
+        if (locationName !== '') {
+            nameQuery = `&name=${locationName}`;
+        }
+    }
+
+    if (type !== undefined) {
+        if (type !== 'Type') {
+            typeQuery = `&type=${type}`;
+        }
+    }
+    
+    if (dimension !== undefined) {
+        if (dimension !== 'Dimension') {
+            dimensionQuery = `&dimension=${dimension}`;
+        }
+    }
+
+    const returnedSearchQuery = `${nameQuery ? nameQuery : ''}${typeQuery ? typeQuery : ''}${dimensionQuery ? dimensionQuery : ''}`;
+    return { url, returnedSearchQuery };
+}
+
 const characters = {
-    getAllCharacters(pageID) {
-        return fetch(`${baseURL}/character?page=${pageID}`)
-            .then(response => response.json())
-            .then(data => data);
+    getCharacters(url){
+        return fetchURL(url);
+        // return fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => data);
     },
 
-    getCharacter(characterID) {
-        return fetch(`${baseURL}/character/${characterID}`)
-            .then(response => response.json())
-            .then(data => data);
-    },
-
-    getSpecificCharacters(IDs) {
-        return fetch(`${baseURL}/character/${IDs}`)
-            .then(response => response.json())
-            .then(data => data);
+    getCharactersByID(IDs) {
+        return fetchURL(`${baseURL}/character/${IDs}`);
+        // return fetch(`${baseURL}/character/${IDs}`)
+        //     .then(response => response.json())
+        //     .then(data => data);
     }
 };
 
 const locations = {
-    getAllLocations(pageID) {
-        return fetch(`${baseURL}/location?page=${pageID}`)
-            .then(response => response.json())
-            .then(data => data);
+    getLocations(url) {
+        return fetchURL(url);
+        // return fetch(url)
+        //     .then(response => response.json())
+        //     .then(data => data);
     },
 
-    getLocation(locationID) {
-        return fetch(`${baseURL}/location/${locationID}`)
-        .then(response => response.json())
-        .then(data => data);
+    getLocationsByID(locationID) {
+        return fetchURL(`${baseURL}/location/${locationID}`);
+        // return fetch(`${baseURL}/location/${locationID}`)
+        //     .then(response => response.json())
+        //     .then(data => data);
     }
-
-
 };
 
-export {characters, locations};
+function fetchURL(url) {
+    return fetch(url).then(response => response.json()).then(data => data);
+} 
+
+export {characters, locations, configureCharactersURL, configureLocationsURL};
